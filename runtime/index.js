dfad12f9042308388b70ec998fccbdf882b69da1
guyoung <guyoung@aliyun.com>
2026-03-26 15:32:37 +0800

    update

--- runtime/index.js
+++ runtime/index.js
@@ -35,32 +35,31 @@ function loadPackage(pkgName) {
 
 function loadBinding() {
     if (platform === 'win32' && arch === 'x64') {
-        //return loadPackage('@wasm-sandbox/wasm-sandbox-win32-x64-msvc')
-      return loadPackage('@wasm-sandbox/packages/win32-x64-msvc')
+      return loadPackage('@wasm-sandbox/wasm-sandbox.win32-x64-msvc')
     }
 
     if (platform === 'win32' && arch === 'arm64') {
-        return loadPackage('@wasm-sandbox/wasm-sandbox-win32-arm64-msvc')
+        return loadPackage('@wasm-sandbox/wasm-sandbox.win32-arm64-msvc')
     }
 
     if (platform === 'darwin' && arch === 'x64') {
-        return loadPackage('@wasm-sandbox/wasm-sandbox-darwin-x64')
+        return loadPackage('@wasm-sandbox/wasm-sandbox.darwin-x64')
     }
 
     if (platform === 'darwin' && arch === 'arm64') {
-        return loadPackage('@wasm-sandbox/wasm-sandbox-darwin-arm64')
+        return loadPackage('@wasm-sandbox/wasm-sandbox.darwin-arm64')
     }
 
     if (platform === 'linux' && arch === 'x64') {
         return isMusl()
-            ? loadPackage('@wasm-sandbox/wasm-sandbox-linux-x64-musl')
-            : loadPackage('@wasm-sandbox/wasm-sandbox-linux-x64-gnu')
+            ? loadPackage('@wasm-sandbox/wasm-sandbox.linux-x64-musl')
+            : loadPackage('@wasm-sandbox/wasm-sandbox.linux-x64-gnu')
     }
 
     if (platform === 'linux' && arch === 'arm64') {
         return isMusl()
-            ? loadPackage('@wasm-sandbox/wasm-sandbox-linux-arm64-musl')
-            : loadPackage('@wasm-sandbox/wasm-sandbox-linux-arm64-gnu')
+            ? loadPackage('@wasm-sandbox/wasm-sandbox.linux-arm64-musl')
+            : loadPackage('@wasm-sandbox/wasm-sandbox.linux-arm64-gnu')
     }
 
     throw new Error(`Unsupported platform: ${platform}-${arch}`)
